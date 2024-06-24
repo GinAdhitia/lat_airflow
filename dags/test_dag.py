10a36cd0dae6c30d3a3f3857d8980ce6f2e8fabb
Ginanjar Adhitia <g.adhitia@gmail.com>
2024-06-24 17:31:41 +0700

    Sample test dag

---
+++ dags/test_dag.py
@@ -0,0 +1,37 @@
+from airflow import DAG
+from airflow.operators.dummy import DummyOperator
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+# Define the default arguments for the DAG
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2024, 6, 24),
+    'retries': 1
+}
+
+# Instantiate the DAG object
+dag = DAG(
+    'test_dag1',
+    default_args=default_args,
+    description='A simple tutorial DAG',
+    schedule_interval=None,
+)
+
+# Define tasks
+task1 = DummyOperator(
+    task_id='task1',
+    dag=dag,
+)
+
+def print_hello():
+    return 'Hello Airflow!'
+
+task2 = PythonOperator(
+    task_id='task2',
+    python_callable=print_hello,
+    dag=dag,
+)
+
+# Define task dependencies
+task1 >> task2
